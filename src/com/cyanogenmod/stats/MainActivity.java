f0ffbca63518641d199e0ffe8aee30f9f07517b0
kmobs <persiansown@gmail.com>
2010-10-01 00:35:38 -0500

    Fix build
    Change-Id: Icc262405479f5938d5670ddfe63f5530f71acb57

--- src/com/cyanogenmod/stats/MainActivity.java
+++ src/com/cyanogenmod/stats/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
